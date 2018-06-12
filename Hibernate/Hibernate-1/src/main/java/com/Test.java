c50a055db4eb9a592d6e6c0359215046d46bad5a
praveen.somireddy@gmail.com <praveen.somireddy@gmail.com>
2018-06-12 10:42:39 +0530

    hibernate added

--- Hibernate/Hibernate-1/src/main/java/com/Test.java
+++ Hibernate/Hibernate-1/src/main/java/com/Test.java
@@ -15,30 +15,20 @@ public class Test {
 	public static void main(String[] args) {
 
 		// hibernate configuration code
+	
 		
-		StandardServiceRegistry registry;
-		SessionFactory sessionFactory;
 		
-		 registry = new StandardServiceRegistryBuilder()
-		            .configure()
-		            .build();
-
-		        // Create MetadataSources
-		        MetadataSources sources = new MetadataSources(registry);
-
-		        // Create Metadata
-		        Metadata metadata = sources.getMetadataBuilder().build();
-
-		        // Create SessionFactory
-		        sessionFactory = metadata.getSessionFactoryBuilder().build();
-		        
-		        Session session = sessionFactory.openSession();
-		        //session.beginTransaction();
+		Configuration cfg=new Configuration();
+		cfg.configure();
+		
+		SessionFactory sf=cfg.buildSessionFactory();
+		 Session session = sf.openSession();
+		
 		
 		Employee emp=new Employee();
-		emp.setEmpId(10);
-		emp.setEmpName("Praveen");
-		emp.setSalary(23456);
+		emp.setEmpId(12);
+		emp.setEmpName("Ozvitha");
+		emp.setSalary(343434);
 		
 		
 		session.save(emp);
